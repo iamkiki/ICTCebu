c73824f940401ae9287e174b39cec1695ac8b519
iamkiki <tasha@ictcebu.com>
2012-04-06 00:32:39 +0800

    contact form

--- contact.php
+++ contact.php
@@ -95,6 +95,32 @@
         </div><!--/span-->
         <div class="span9">
 			<legend>Contact</legend>
+			<form class="form-horizontal">
+				<fieldset>
+				  <div class="control-group">
+					<label class="control-label" for="input01">Name</label>
+					<div class="controls">
+					  <input type="text" class="input-xlarge" id="input01">
+					</div>
+				  </div>
+				  <div class="control-group">
+					<label class="control-label" for="input02">Email Address</label>
+					<div class="controls">
+					  <input type="text" class="input-xlarge" id="input02">
+					</div>
+				  </div>
+				  <div class="control-group">
+					<label class="control-label" for="textarea">Message</label>
+					<div class="controls">
+					  <textarea class="input-xlarge" id="textarea" rows="12"></textarea>
+					</div>
+				  </div>
+				  <div class="form-actions">
+					<button type="submit" class="btn btn-danger">Submit</button>
+					<button class="btn">Cancel</button>
+				  </div>
+				</fieldset>
+			  </form>
 		</div><!--/span-->
       </div><!--/row-->
       <hr>
