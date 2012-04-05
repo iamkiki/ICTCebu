fe88d4796665ec43fa63198b0d3a803bd252da81
iamkiki <tasha@ictcebu.com>
2012-04-06 00:01:44 +0800

    jobs page

---
+++ jobs.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>ICTCebu IT-BPO Company and Jobs Directory</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+	  .gray {
+		color: gray;
+	  }
+	  .red {
+		color: #DA4F49;
+	  }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#"><span class="red"><strong>ICTCebu</strong></span><span class="gray">.com</span></a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li><a href="index.php">Home</a></li>
+			  <li><a href="companies.php">Companies</a></li>
+			  <li class="active"><a href="jobs.php">Jobs</a></li>
+              <li><a href="about.php">About</a></li>
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+			<form class="navbar-search pull-left">
+			  <input type="text" class="search-query" placeholder="Search">
+			</form>
+			<p class="navbar-text pull-right"><a href="#">Register Company</a> | <a href="#">Login</a></p>
+            <!--<p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>-->
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Browse Categories</li>
+              <li class="active"><a href="#">BPO/ Call Centers</a></li>
+              <li><a href="#">Web/ Mobile Development</a></li>
+              <li><a href="#">Software Applications</a></li>
+              <li><a href="#">Hardware/ Peripherals</a></li>
+			  <li><a href="#">Others</a></li>
+			  
+			  <li class="nav-header">Hot Jobs</li>
+              <li><a href="#">Programmer</a></li>
+              <li><a href="#">Web Developer</a></li>
+              <li><a href="#">Technical Support</a></li>
+              <li><a href="#">Data Encoder</a></li>
+			  <li><a href="#">Programmer</a></li>
+              <li><a href="#">Web Developer</a></li>
+              <li><a href="#">Technical Support</a></li>
+              <li><a href="#">Data Encoder</a></li>
+			  <li><a href="#">Technical Support</a></li>
+              <li><a href="#">Data Encoder</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <div class="span9">
+		  <div class="row-fluid">
+			<h1 class="d-inline f-left">Jobs</h1>
+			<div class="btn-group d-inline f-left m-left10 m-bottom5 m-top3">
+			  <button class="btn btn-danger">All Categories</button>
+			  <button class="btn btn-danger dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
+			  <ul class="dropdown-menu">
+				<li><a href="#">All Categories</a></li>
+				<li><a href="#">BPO/ Call Centers</a></li>
+				<li><a href="#">Web/ Mobile Development</a></li>
+                <li><a href="#">Software Applications</a></li>
+                <li><a href="#">Hardware/ Peripherals</a></li>
+			    <li><a href="#">Others</a></li>
+			  </ul>
+			</div>
+		  </div>
+          <div class="row-fluid">
+			<table class="table">
+				<thead>
+				  <tr>
+					<th>Date</th>
+					<th>Position</th>
+					<th>Experience</th>
+					<th>Company</th>
+					<th>Location</th>
+				  </tr>
+				</thead>
+				<tbody>
+				  <tr>
+					<td>April 1</td>
+					<td>Web Developer</td>
+					<td>1 yr</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Technical Support Specialist</td>
+					<td>2 yrs</td>
+					<td>Number One Call Center</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Java Developer</td>
+					<td>2 yrs</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Web Developer</td>
+					<td>1 yr</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Technical Support Specialist</td>
+					<td>2 yrs</td>
+					<td>Number One Call Center</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Java Developer</td>
+					<td>2 yrs</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Web Developer</td>
+					<td>1 yr</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Technical Support Specialist</td>
+					<td>2 yrs</td>
+					<td>Number One Call Center</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>Java Developer</td>
+					<td>2 yrs</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				  <tr>
+					<td>April 1</td>
+					<td>PHP Developer</td>
+					<td>3 yrs</td>
+					<td>Nerubia Web Solutions, Inc.</td>
+					<td>Lahug, Cebu City</td>
+				  </tr>
+				</tbody>
+			</table>
+          </div><!--/row-->
+        </div><!--/span-->
+      </div><!--/row-->
+	  <div class="pagination f-right">
+		  <ul>
+			<li><a href="#">&larr;</a></li>
+			<li class="active">
+			  <a href="#">1</a>
+			</li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">&rarr;</a></li>
+		  </ul>
+	  </div>
+      <hr>
+
+      <footer>
+        <p>&#169; <?php echo date('Y', time()); ?> ictcebu.com | Email us at <a href="/contact">info@ictcebu.com</a></p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
