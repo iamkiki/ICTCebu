89b9d95f2c8632c9ff02584d3e12ce59742f53a6
iamkiki <tasha@ictcebu.com>
2012-05-02 19:46:38 +0800

    ckeditor

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -1,7 +1,57 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Jobs extends CI_Controller {
+    
+    function __construct()
+    {
+        parent::__construct();
 
+        $this->load->helper('url'); //You should autoload this one ;)
+        $this->load->helper('ckeditor');
+
+        //Ckeditor's configuration
+        $this->data['ckeditor'] = array(
+
+            //ID of the textarea that will be replaced
+            'id'     =>     'content',
+            'path'    =>    base_url().'scripts/ckeditor',
+
+            //Optionnal values
+            'config' => array(
+                'toolbar'     =>     "Full",     //Using the Full toolbar
+                'width'     =>     "500px",    //Setting a custom width
+                'height'     =>     '300px',    //Setting a custom height
+
+            ),
+
+            //Replacing styles from the "Styles tool"
+            'styles' => array(
+
+                //Creating a new style named "style 1"
+                'style 1' => array (
+                    'name'         =>     'Blue Title',
+                    'element'     =>     'h2',
+                    'styles' => array(
+                        'color'             =>     'Blue',
+                        'font-weight'         =>     'bold'
+                    )
+                ),
+
+                //Creating a new style named "style 2"
+                'style 2' => array (
+                    'name'         =>     'Red Title',
+                    'element'     =>     'h2',
+                    'styles' => array(
+                        'color'             =>     'Red',
+                        'font-weight'         =>     'bold',
+                        'text-decoration'    =>     'underline'
+                    )
+                )                
+            )
+        );
+
+    }
+    
     public function index()
     {
     $this->load->view('jobs');
@@ -14,7 +64,16 @@ class Jobs extends CI_Controller {
     public function post(){
         if($this->session->userdata('auth')){
             $this->a_outer['a_js'][] = 'post';
-            $this->load->view('user');
+            $this->load->helper('ckeditor',base_url() . 'scripts/ckeditor/');
+            $this->ckeditor->basePath = base_url(). 'scripts/ckeditor/';
+            $this->ckeditor->ToolbarSet = 'Full';
+            $a_data['ckeditor'] = $this->data;
+            
+            $a_content = $this->m_contents->load( array('id' => $i_article_id, 'is_active' => 1) );
+            
+            $a_data['o_content'] = $a_content->row();
+            
+            $this->load->view('user', array('a_data' => $a_data));
         } else {
             header('Location: /');
         }
---
+++ js/ckeditor/config.js
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
+
+CKEDITOR.on('instanceReady', function( ev ) {
+  var blockTags = ['div','h1','h2','h3','h4','h5','h6','p','pre','li','blockquote','ul','ol',
+  'table','thead','tbody','tfoot','td','th',];
+
+  for (var i = 0; i < blockTags.length; i++)
+  {
+     ev.editor.dataProcessor.writer.setRules( blockTags[i], {
+        indent : false,
+        breakBeforeOpen : false,
+        breakAfterOpen : false,
+        breakBeforeClose : false,
+        breakAfterClose : true
+     });
+  }
+});
